e25ba3a943d4bbf96c7624800e03bcd7575bbdfd
Ayesha Khan <khan59@fred.socs.uoguelph.ca>
2023-03-28 03:13:09 -0400

    Corrected implementation

--- src/printAll.c
+++ src/printAll.c
@@ -1,29 +1,25 @@
 #include "../include/headerA3.h"
 
-void printAll(struct employee *headLL) {
+void printAll(struct employee* headLL) {
+    struct employee* current = headLL;
+    int numEmp = 0;
 
-    // Check if the LL is empty
-    if (headLL == NULL) {
-    printf("No employees to display.\n");
-    return;
-    }
-
-    // Traverse the LL and print employee information
-    struct employee *currentEmp = headLL;
-    printf("Employee List:\n");
-    printf("%-10s %-10s %-10s\n", "Employee", "First", "Last");
-    printf("%-10s %-10s %-10s\n", "ID", "Name", "Name");
-    while (currentEmp != NULL) {
-        printf("%-10d %-10s %-10s\n", currentEmp->empId, currentEmp->fname, currentEmp->lname);
-        // Print the names of the dependents
-        printf("Dependents [%d]: ", currentEmp->numDependents);
-        for (int i = 0; i < currentEmp->numDependents; i++) {
-            printf("%s", currentEmp->dependents[i]);
-            if (i != currentEmp->numDependents-1) {
+    while (current != NULL) {
+        numEmp++;
+        printf("Employee # %d:\n", numEmp);
+        printf("Employee id: %d\n", current->empId);
+        printf("First name: %s\n", current->fname);
+        printf("Last name: %s\n", current->lname);
+        printf("Dependents [%d]: ", current->numDependents);
+        for (int i = 0; i < current->numDependents; i++) {
+            printf("%s", current->dependents[i]);
+            if (i != current->numDependents - 1) {
                 printf(", ");
             }
         }
         printf("\n");
-        currentEmp = currentEmp->nextEmployee;
+        current = current->nextEmployee;
     }
+
+    printf("Currently, there are %d employees.\n", numEmp);
 }
